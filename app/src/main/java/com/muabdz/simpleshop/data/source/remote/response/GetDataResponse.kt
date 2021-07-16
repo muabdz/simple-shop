5343becffea5b16c57c7ac6db3bdb5eba1da6fda
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-16 23:16:43 +0700

    add home data response model

---
+++ app/src/main/java/com/muabdz/simpleshop/data/source/remote/response/GetDataResponse.kt
@@ -0,0 +1,57 @@
+package com.muabdz.simpleshop.data.source.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GetDataResponse(
+
+	@field:SerializedName("GetDataResponse")
+	val getDataResponse: List<GetDataResponseItem>
+)
+
+data class Data(
+
+	@field:SerializedName("productPromo")
+	val productPromo: List<ProductPromoItem>,
+
+	@field:SerializedName("category")
+	val category: List<CategoryItem>
+)
+
+data class ProductPromoItem(
+
+	@field:SerializedName("loved")
+	val loved: Int,
+
+	@field:SerializedName("price")
+	val price: String,
+
+	@field:SerializedName("imageUrl")
+	val imageUrl: String,
+
+	@field:SerializedName("description")
+	val description: String,
+
+	@field:SerializedName("id")
+	val id: String,
+
+	@field:SerializedName("title")
+	val title: String
+)
+
+data class GetDataResponseItem(
+
+	@field:SerializedName("data")
+	val data: Data
+)
+
+data class CategoryItem(
+
+	@field:SerializedName("imageUrl")
+	val imageUrl: String,
+
+	@field:SerializedName("name")
+	val name: String,
+
+	@field:SerializedName("id")
+	val id: Int
+)
