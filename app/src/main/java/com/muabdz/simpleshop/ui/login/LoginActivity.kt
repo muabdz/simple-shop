92b2590e2872a8a6a4d1848ce60b4a36b748f51a
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-18 11:25:59 +0700

    implement facebook auth from login activity

--- app/src/main/java/com/muabdz/simpleshop/ui/login/LoginActivity.kt
+++ app/src/main/java/com/muabdz/simpleshop/ui/login/LoginActivity.kt
@@ -1,8 +1,14 @@
 package com.muabdz.simpleshop.ui.login
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginResult
+import com.muabdz.simpleshop.R
 import com.muabdz.simpleshop.databinding.ActivityLoginBinding
 import com.muabdz.simpleshop.ui.home.HomeActivity
 import com.orhanobut.hawk.Hawk
@@ -10,17 +16,47 @@ import com.orhanobut.hawk.Hawk
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var viewBinding: ActivityLoginBinding
-
+    private lateinit var callbackManager: CallbackManager
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        viewBinding.btnSignIn.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
         Hawk.init(this).build()
+
+        viewBinding.btnSignIn.setOnClickListener {
+            navigateToHome()
+        }
+        callbackManager = CallbackManager.Factory.create()
+        viewBinding.btnLoginFb.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(result: LoginResult?) {
+                navigateToHome()
+            }
+
+            override fun onCancel() {
+                showLoginError()
+            }
+
+            override fun onError(error: FacebookException?) {
+                showLoginError()
+            }
+        })
+
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun navigateToHome() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun showLoginError() {
+        Toast.makeText(this, getString(R.string.wording_login_failed), Toast.LENGTH_SHORT).show()
+    }
+
 }
