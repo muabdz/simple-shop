094424ba702b8abc48c23b2692dff8862c1ba7c9
Mu'adz Abdurrahman <muuadzz10@gmail.com>
2021-07-18 11:25:24 +0700

    add facebook login UI

--- app/src/main/java/com/muabdz/simpleshop/ui/MyApplication.kt
+++ app/src/main/java/com/muabdz/simpleshop/ui/MyApplication.kt
@@ -1,10 +1,16 @@
 package com.muabdz.simpleshop.ui
 
 import android.app.Application
+import com.facebook.appevents.AppEventsLogger
 import com.muabdz.simpleshop.di.AppComponent
 import com.muabdz.simpleshop.di.DaggerAppComponent
 
 class MyApplication: Application() {
 
     val appComponent: AppComponent = DaggerAppComponent.create()
+
+    override fun onCreate() {
+        super.onCreate()
+        AppEventsLogger.activateApp(this)
+    }
 }
